32b8b3b51b6551f3aa6561022b09b6056b7ce5b4
twhughes <tylerwhughes91@gmail.com>
2020-04-11 15:56:32 -0400

    remove comments

--- workout/load.py
+++ workout/load.py
@@ -24,22 +24,3 @@ def strip_section(section_dict):
     routine = routine_map[routine_name]
     exercises = section_dict['exercises']
     return section(name=name, exercises=exercises, routine=routine)
-
-# # import routines (how to execute each list of exercises)
-# from workout.routines import one_min_each, abab_25
-
-# # define the different `sections` of your workout with a list of exercises and a routine for running them
-# warmup   = section(name='warmup',   exercises=warmup_exercises,   routine=one_min_each)
-# hit      = section(name='HIT',      exercises=hit_exercises,      routine=abab_25)
-# cooldown = section(name='cooldown', exercises=cooldown_exercises, routine=one_min_each)
-
-# # create a workout defined by a list of sections
-# hard_workout = workout(name='hard', sections=[warmup, hit, cooldown])
-
-# # run the workout
-# hard_workout.run()
-
-
-    # print(name)
-    # print(sections)
-
